5aec4b30910c470d9d567e622c4fb4d1c3b3d183
unknown <Teddy_03@outlook.es>
2023-05-16 22:49:45 -0600

    4 Commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //Se importa la carpeta bootstrap
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css' 
+import '/src/bootstrap-grid.min.css' 
 //**************** */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
