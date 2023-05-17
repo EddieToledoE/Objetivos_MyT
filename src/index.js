d30133eb343909bc8757c126c4d6f25667b540a7
unknown <Teddy_03@outlook.es>
2023-05-16 22:47:05 -0600

    3 Commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //Se importa la carpeta bootstrap
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css' 
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css' 
 //**************** */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
